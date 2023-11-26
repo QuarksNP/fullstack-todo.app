e6f76ff36c26ea40e2a020d4d6f5272a9317dac6
Alberto_gzb <93887130+QuarksNP@users.noreply.github.com>
2023-11-25 21:49:11 -0400

    implementing redirection

--- client/src/features/user/auth/login.component.tsx
+++ client/src/features/user/auth/login.component.tsx
@@ -1,30 +1,33 @@
-import { Form, Button, Field } from "@features/ui";
+import { Form, Button, Field, Redirect } from "@features/ui";
 
 import { handlePostUser } from "../handle-post-user.util";
 
 export const Login = () => {
-
   return (
-    <article className="md:p-10">
-      <h1 className="text-3xl font-bold mb-10 text-center">Welcome to Todo-app 📝</h1>
+    <article className="w-full p-10">
+      <h1 className="text-3xl font-bold mb-10 text-center">
+        Welcome to Todo-app 📝
+      </h1>
 
-      <Form
-        handleSubmit={(e) => handlePostUser(e, "login")}
-        method="post"
-      >
+      <Form handleSubmit={(e) => handlePostUser(e, "login")} method="post">
         <fieldset>
           <label htmlFor="username">Username</label>
-          <Field name="username" id="username" required/>
+          <Field name="username" id="username" required />
         </fieldset>
 
         <fieldset>
           <label htmlFor="password">Password</label>
-          <Field name="login-pas" id="password" required/>
+          <Field name="login-pas" id="password" required />
         </fieldset>
 
-          <Button {...{ intent: "active" }}>Login</Button>
-       
+        <Button {...{ intent: "active" }}>Login</Button>
       </Form>
+
+      <Redirect to="/sign-up" question="Do not you have an account?">
+        <button className="text-BrightBlue hover:underline">
+          Create account
+        </button>
+      </Redirect>
     </article>
   );
 };
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,5 +1,4 @@
 import { CoverImage, Login } from "@features/user"
-
 const LoginPage = () => {
     return(
         <section className="md:grid md:grid-cols-2 bg-DarkGray w-full h-full rounded flex flex-col items-center justify-center text-white md:h-auto md:shadow-xl md:m-auto">
